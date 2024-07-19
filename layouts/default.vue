2b26e52d26a2e7be41ee1aacad13af39a558bd5b
joelreid <joel.reid@theskillsnetwork.com>
2024-07-19 12:59:55 +0100

    set up base routes and the how to guide including setting difficulty.

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Navbar/>
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  color:white;
+}
+</style>
